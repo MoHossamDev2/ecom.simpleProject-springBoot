dcf38a475a08503a61f9c4e6bcf7622c6587b7c8
Hossam <mhsam2916@gmail.com>
2025-05-02 18:12:22 +0300

    Initial commit

---
+++ src/test/java/com/telusko/ecom/proj/repo/ProductRepoTest.java
@@ -0,0 +1,11 @@
+package com.telusko.ecom.proj.repo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepoTest {
+    @Test
+    void test() {}
+
+}
